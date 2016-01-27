a0c0e52eaf04826a59881a0a9720ad3b63d67e95
Dimitris Mouris <jimmouris@hotmail.com>
2016-01-28 01:25:57 +0200

    Add church Numbers

--- lci.hs
+++ lci.hs
@@ -1,11 +1,33 @@
+{- Lambda Calculus Interpreter -}
 
 {- Church Numerals -}
-c0 = \f x -> x
-c1 = \f x -> f x
-c2 = \f x -> f (f x)
+type Church a = (a -> a) -> a -> a
 
-{- succ(n) = n + 1 -}
-succ = \n f x -> f (n f x)
+church :: Integer -> Church Integer
+church 0 = \f -> \x -> x
+church n = \f -> \x -> f (church (n-1) f x)
 
-{- plus(m, n) = m + n -}
-plus = \m n f x -> m f (n f x)
+unchurch :: Church Integer -> Integer
+unchurch cn = cn (+ 1) 0
+
+--c0 = \f x -> x
+--c1 = \f x -> f x
+--c2 = \f x -> f (f x)
+
+{- lsucc(n) = n + 1 -}
+lsucc = \n f x -> f (n f x)
+
+{- lplus(m, n) = m + n -}
+lplus = \m n f x -> m f (n f x)
+
+{- lmult(m, n) = m * n -}
+lmult = \m n f -> m (n f)
+
+{- lexp(m, n) = m ^ n -}
+lexp = \m n -> n m
+
+
+{- Main -}
+main :: IO ()
+main = do let z = unchurch (lsucc (church 4))
+          putStrLn $ "The result is: " ++ show z

4da1fe91f48e38591ebbf812fd5896848c2b6597
Dimitris Mouris <jimmouris@hotmail.com>
2016-02-01 13:42:05 +0200

    Count bounded variables

--- lci.hs
+++ lci.hs
@@ -29,20 +29,27 @@ data Result = Res Term Int [Term] [String] deriving(Show, Eq)
 --freeVars (Application t1 t2) = (freeVars t1 ++ freeVars t2)
 --freeVars (Abstraction s1 t2) = (freeVars t1 ++ freeVars t2)
 
--------------------- Rename -----------------------
+boundVars :: Term -> [String]
+boundVars (Var v) = []
+boundVars (Application t1 t2) = boundVars t1 ++ boundVars t2
+boundVars (Abstraction s1 t2) = [s1] ++ boundVars t2
+
 renameVarinTerm :: Term -> String -> String -> Term
 renameVarinTerm (Var s1) s s' = if (s1 == s) then (Var s') else (Var s1)
 renameVarinTerm (Abstraction s1 t) s s' = if (s1 == s) then (Abstraction s' (renameVarinTerm t s s')) else (Abstraction s1 (renameVarinTerm t s s'))
 renameVarinTerm (Application t1 t2) s s' = (Application (renameVarinTerm t1 s s') (renameVarinTerm t2 s s'))
 
-------- Alpha reduce applies only to abstraction ------
-alphaReduce :: Term -> Term
-alphaReduce (Abstraction s t) = (Abstraction s' t') where
+------- Alpha reduce applies only to abstraction! ------
+alphaReduction :: Term -> Term
+alphaReduction (Abstraction s t) = (Abstraction s' t') where
 	s' = s ++ "'"
 	t' = renameVarinTerm t s s'
 
+betaReduction :: Term -> Term
+betaReduction x = x
+
 reduce :: Term -> Term
-reduce x = x
+reduce t = t
 
 --------------------------------------- PARSER --------------------------------------------
 lambdaTerm :: Parser Term
